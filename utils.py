fd13345b40e1519cf73c6ff8b64fbc860a05c83d
Soldat <soldasim@fel.cvut.cz>
2022-12-31 11:48:42 +0100

    refactor gist comment request

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import os
+import re
 from enum import Enum
 
-GIT_TOKEN = "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"
+GIT_TOKEN = "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"
 
 
 class Command(Enum):
@@ -11,16 +12,15 @@ class Command(Enum):
 
 
 def curl_post_gist_comment(msg):
-    cmd = """
-            curl \
-                -X POST \
-                -H 'Accept: application/vnd.github+json' \
-                -H 'Authorization: Bearer """ + GIT_TOKEN + """' \
-                -H 'X-GitHub-Api-Version: 2022-11-28' \
-                https://api.github.com/gists/081b69780c34110b4acff6c6ba39bc88/comments \
-                -d "{'body':'""" + msg + """'}"
-        """
-
+    cmd = "curl \
+            -X POST \
+            -H 'Accept: application/vnd.github+json' \
+            -H 'Authorization: Bearer " + GIT_TOKEN + "' \
+            -H 'X-GitHub-Api-Version: 2022-11-28' \
+            https://api.github.com/gists/081b69780c34110b4acff6c6ba39bc88/comments \
+            -d '{\"body\":\"" + msg + "\"}' \
+        \n"
+    cmd = re.sub(' +', ' ', cmd)
     response = perform_command(cmd)
     return response
 
